908b9067c2c15f4c0f76216298b92a82a639d688
Tanvir Walia <tarywalia@gmail.com>
2020-03-07 04:25:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,105 +1,76 @@
 # Constants help us "name" special numbers
 
-CLEARANCE_ITEM_DISCOUNT_RATE = 0.20
-BIG_PURCHASE_DISCOUNT_RATE = 0.10
-
 def find_item_by_name_in_collection(name, collection)
-  i = 0
-  while i < collection.length do
-    return collection[i] if name === collection[i][:item]
-    i += 1
-  end
-  nil
+  i = 0 # 0 is the first index of any array
+
+while i < collection.length
+    if collection[i][:item] == name # calling on our array and using the idex operator ,
+                                    # accesing an item in our collection
+        return collection[i] # it will try to find the item in the collection, find the matching name
+    end
+  i += 1
 end
+nil # ensures nil gets returned if while loops complete without finding an item
+end
+
 
 def consolidate_cart(cart)
+  # code here
+  new_cart = [] # new_cart variable, [] can fill with items now
   i = 0
-  result = []
-
-  while i < cart.count do
-    item_name = cart[i][:item]
-    sought_item = find_item_by_name_in_collection(item_name, result)
-    if sought_item
-      sought_item[:count] += 1
-    else
-      cart[i][:count] = 1
-      result << cart[i]
-    end
-    i += 1
+    while i < cart.length
+      unique_item = find_item_by_name_in_collection(cart[i][:item],new_cart) # taking method from above, taking each item in the cart
+      ## and see if its our new cart to see if it already exists
+      if  unique_item
+        unique_item[:count] +=1 # will increase by 1 if item is not nil and access the count of the item
+      else # if the item is equal nil, youd have to create a new item
+      unique_item ={
+        :item => cart[i][:item], # construct item for new cart,
+        :price => cart[i][:price],
+        :clearance => cart[i][:clearance],
+        :count => 1 #
+      }
+      #once new hash is built, add new hash to new cart
+      new_cart << unique_item
+      end
+      i +=1
   end
-
-  result
-end
-
-# Don't forget, you can make methods to make your life easy!
-
-def mk_coupon_hash(c)
-  rounded_unit_price = (c[:cost].to_f * 1.0 / c[:num]).round(2)
-  {
-    :item => "#{c[:item]} W/COUPON",
-    :price => rounded_unit_price,
-    :count => c[:num]
-  }
-end
-
-# A nice "First Order" method to use in apply_coupons
-
-def apply_coupon_to_cart(matching_item, coupon, cart)
-  matching_item[:count] -= coupon[:num]
-  item_with_coupon = mk_coupon_hash(coupon)
-  item_with_coupon[:clearance] = matching_item[:clearance]
-  cart << item_with_coupon
+new_cart
 end
 
 def apply_coupons(cart, coupons)
-  i = 0
-  while i < coupons.count do
-    coupon = coupons[i]
-    item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
-    item_is_in_basket = !!item_with_coupon
-    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+  counter = 0
 
-    if item_is_in_basket and count_is_big_enough_to_apply
-      apply_coupon_to_cart(item_with_coupon, coupon, cart)
+  while counter < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[counter][:num]
+        cart_item[:count] -= coupons[counter][:num]
+      else
+        cart_item_with_coupon = {
+          :item => couponed_item_name,
+          :price => coupons[counter][:cost]/ coupons[counter][:num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
     end
-    i += 1
-  end
 
+    end
+    counter +=1
+  end
   cart
 end
 
 def apply_clearance(cart)
-  i = 0
-  while i < cart.length do
-    item = cart[i]
-    if item[:clearance]
-      discounted_price = ((1 - CLEARANCE_ITEM_DISCOUNT_RATE) * item[:price]).round(2)
-        item[:price] = discounted_price
-    end
-    i += 1
-  end
-
-  cart
+  # code here
 end
 
 def checkout(cart, coupons)
-  total = 0
-  i = 0
-
-  ccart = consolidate_cart(cart)
-  apply_coupons(ccart, coupons)
-  apply_clearance(ccart)
-
-  while i < ccart.length do
-    total += items_total_cost(ccart[i])
-    i += 1
-  end
-
-  total >= 100 ? total * (1.0 - BIG_PURCHASE_DISCOUNT_RATE) : total
-end
-
-# Don't forget, you can make methods to make your life easy!
-
-def items_total_cost(i)
-  i[:count] * i[:price]
+  # code here
 end
